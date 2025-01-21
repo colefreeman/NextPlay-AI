c6a1b7897e8bd22f1fe194bea2bfd2c9dadcf1c5
Cole Freeman <cole.f.freeman@gmail.com>
2025-01-21 07:05:14 -0500

    navbar fixes for logo

--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -1,12 +1,15 @@
 // src/components/Layout/Navbar.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/nextplay-logo-white.svg"; // Adjust the path if needed
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Your App Name</Link>
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="App Logo" className="logo" />
+        </Link>
       </div>
       <div className="navbar-menu">
         <Link to="/profile">Profile</Link>
